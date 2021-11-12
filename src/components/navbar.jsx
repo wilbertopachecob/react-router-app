d3a51900068959a83c19aa61479bc626999f676d
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-12 13:08:54 -0600

    inserted nav element to fix style

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,20 +3,40 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/products">Products</Link>
-      </li>
-      <li>
-        <Link to="/posts/2018/06">Posts</Link>
-      </li>
-      <li>
-        <Link to="/admin">Admin</Link>
-      </li>
-    </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Router App
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/products">Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/posts/2018/06">Posts</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
