186a3922f25c4740100d72d6c2137066d4ccb2e4
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-12 13:10:01 -0600

    added boostrap card to contain links

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -6,21 +6,23 @@ class Products extends Component {
     products: [
       { id: 1, name: "Product 1" },
       { id: 2, name: "Product 2" },
-      { id: 3, name: "Product 3" }
-    ]
+      { id: 3, name: "Product 3" },
+    ],
   };
 
   render() {
     return (
       <div>
         <h1>Products</h1>
-        <ul>
-          {this.state.products.map(product => (
-            <li key={product.id}>
-              <Link to={`/products/${product.id}`}>{product.name}</Link>
-            </li>
-          ))}
-        </ul>
+        <div className="card" style={{ width: "18rem" }}>
+          <ul className="list-group list-group-flush">
+            {this.state.products.map((product) => (
+              <li className="list-group-item" key={product.id}>
+                <Link to={`/products/${product.id}`}>{product.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
