59568f4941b63eff239c622a3425692b1d48c4d0
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-12 13:09:16 -0600

    added class to button

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 
 class ProductDetails extends Component {
   handleSave = () => {
-    // Navigate to /products
-    this.props.history.replace('/products');
+    this.props.history.replace("/products");
   };
 
   render() {
     return (
       <div>
         <h1>Product Details - {this.props.match.params.id}</h1>
-        <button onClick={this.handleSave}>Save</button>
+        <button className="btn btn-primary" onClick={this.handleSave}>
+          Save
+        </button>
       </div>
     );
   }
