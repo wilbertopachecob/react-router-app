b25bef3bef4240c4855052ffe286b7ac971299d9
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-12 11:17:45 -0600

    added switch to App.js to only match one route

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+// import ProductDetails from "./components/productDetails";
+// import NotFound from "./components/notFound";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/posts" component={Posts}></Route>
+            <Route path="/products" component={Products}></Route>
+            <Route path="/admin" component={Dashboard}></Route>
+            <Route path="/" component={Home}></Route>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
