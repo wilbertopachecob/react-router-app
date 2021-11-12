dd0d32f144f43c5183c55290779282f6da3d2369
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-12 11:49:09 -0600

    added route params

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
-// import ProductDetails from "./components/productDetails";
+import ProductDetails from "./components/productDetails";
 // import NotFound from "./components/notFound";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
@@ -16,7 +16,9 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
+            <Route path="/posts/:year/:month" component={Posts}></Route>
             <Route path="/posts" component={Posts}></Route>
+            <Route path="/products/:id" component={ProductDetails}></Route>
             <Route path="/products" render={(props) => <Products sortBy="newest" {...props} />}></Route>
             <Route path="/admin" component={Dashboard}></Route>
             <Route path="/" component={Home}></Route>
