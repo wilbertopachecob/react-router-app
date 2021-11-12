f4ea55bf06b37ae453ceba2c7cb4cde5950b60ca
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-12 12:25:37 -0600

    introduced Redirect

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
-// import NotFound from "./components/notFound";
+import NotFound from "./components/notFound";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -18,13 +18,16 @@ class App extends Component {
           <Switch>
             <Route path="/posts/:year?/:month?" component={Posts}></Route>
             <Route path="/posts" component={Posts}></Route>
+            <Redirect from="/messages" to="/posts"  />
             <Route path="/products/:id" component={ProductDetails}></Route>
             <Route
               path="/products"
               render={(props) => <Products sortBy="newest" {...props} />}
             ></Route>
             <Route path="/admin" component={Dashboard}></Route>
-            <Route path="/" component={Home}></Route>
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home}></Route>
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
