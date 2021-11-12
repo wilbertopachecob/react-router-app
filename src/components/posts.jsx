b85088e8d9a4f5318a0c52c4d61dd8fb9dffef50
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-12 12:25:52 -0600

    fixed error with queryParams

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import queryString from "query-string";
 
 const Posts = ({ match, location }) => {
-  const result = queryString(location.search);
+  const result = queryString.parse(location.search);
   return (
     <div>
       <h1>Posts</h1>
